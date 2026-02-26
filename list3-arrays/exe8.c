a2062a62f99f6a5460aca000fe9aa0f92976e6d0
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-02-26 14:43:27 -0300

    add exercise 8 from array list

---
+++ list3-arrays/exe8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+  
+}
+
+void main() {
+  int num;
+
+  printf("Insira um número para saber se ele é primo ou não: ");
+  scanf("%d", &num);
+
+  if (isPrime(num)) printf("O número %d é primo.", num);
+  else printf("O número %d é primo.", num);
+}
