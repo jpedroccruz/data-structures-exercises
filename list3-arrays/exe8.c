8e312ccf3de277128385c62ca5f61316a34c9024
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-03-03 21:27:58 -0300

    update exercise 8 from array list

--- list3-arrays/exe8.c
+++ list3-arrays/exe8.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
 int isPrime(int num) {
-  for (int i = num - 1; i >= 2; i--) { 
-    if (num % i == 0) return 0;
-  }
+  if (num % 2 == 0) return 0;
+
+  for (int i = num - 1; i >= 3; i -= 2) if (num % i == 0) return 0;
 
   return 1;
 }
 
 void main() {
-  int num = 5000, array[10], i = 0;
+  int num = 5000, array[20], i = 0;
 
-  while (i < 10) {
-    if (isPrime(num)) {
-      array[i] = num;
-      i++;
-    } 
+  while (i < 20) {
+    if (isPrime(num)) array[i++] = num;
 
     num++;
   }
 
   printf("Lista dos 20 primeiros números primos começando pelo 5000: ");
 
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 20; i++) {
     printf("%d ", array[i]);
   }
+  
+  printf("\n");
 }
