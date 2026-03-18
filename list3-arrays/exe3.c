4bb5261007a596d6f75e8b87514b6850c344455b
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-03-18 14:36:15 -0300

    update exercise 3 from arrays list to use dinamic allocation

--- list3-arrays/exe3.c
+++ list3-arrays/exe3.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int veryfiId(int id, int *array) {
-  for (int i = 0; i < 10; i++) if (id == array[i]) return 1;
+int veryfiId(int id, int *array, int n) {
+  for (int i = 0; i < n; i++) if (id == array[i]) return 1;
 
   return 0;
 }
 
 void main() {
-  int array[10], aux_var, i;
+  int n, aux_var, i;
 
-  for (i = 0; i < 10; i++) {
+  printf("Insira a quantidade de alunos que vão ser inseridos: ");
+  scanf("%d", &n);
+
+  int *array = calloc(sizeof (int), n);
+
+  for (i = 0; i < n; i++) {
     do {
       printf("Insira o número de matrícula do aluno %d: ", i + 1);
       scanf("%d", &aux_var);
-    } while (veryfiId(aux_var, array) == 1);
+    } while (veryfiId(aux_var, array, n) == 1);
 
     array[i] = aux_var;
   }
