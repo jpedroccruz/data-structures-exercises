2f9774c6ac7192f861735907a42e3a9d4dcf1f03
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-05-20 15:06:24 -0300

    update implementations of binary tree

--- binary-trees/first-implementation.c
+++ binary-trees/first-implementation.c
@@ -17,19 +17,29 @@ void addNode(Node **root, int value) {
   *root = new_node;
 }
 
-void printTree(Node *tree) {
+Node *createNode(int value) {
+  Node *new_node = malloc(sizeof(Node));
+
+  new_node->value = value;
+  new_node->left = NULL;
+  new_node->right = NULL;
+
+  return new_node;
+}
+
+void printTree(Node *tree, int level) {
   if (tree == NULL) {
-    printf("Árvore vazia.");
     return;
   }
 
   // pré ordem
   // em ordem
   // pós ordem
-  
-  printTree(tree->left);
-  printTree(tree->right);
+
+  printTree(tree->left, level + 1);
+  for (int i = level * 5; i >= 0; i--) printf(" ");
   printf("%d\n", tree->value);
+  printTree(tree->right, level + 1);
 }
 
 int countTreeNodes(Node *tree) {
@@ -86,20 +96,46 @@ void clearTree(Node **tree) {
   *tree = NULL;
 }
 
+void insertElement(Node **root, int value) {
+  if (*root == NULL) {
+    *root = createNode(value);
+    return;
+  }
+
+  if (value < (*root)->value) insertElement(&(*root)->left, value);
+  else insertElement(&(*root)->right, value);
+
+  return;
+}
+
+Node *getMaximumNode(Node *root) {
+  if (root == NULL) return NULL;
+  if (root->right == NULL) return root;
+  return getMaximumNode(root);
+}
+
 int main(void) {
   Node *root = NULL;
 
-  addNode(&root, 50);
+  // addNode(&root, 50);
 
-  addNode(&root->left, 30);
-  addNode(&root->left->left, 20);
-  addNode(&root->left->right, 40);
-  addNode(&root->left->right->right, 45);
+  // addNode(&root->left, 30);
+  // addNode(&root->left->left, 20);
+  // addNode(&root->left->right, 40);
+  // addNode(&root->left->right->right, 45);
 
-  addNode(&root->right, 70);
-  addNode(&root->right->right, 85);
+  // addNode(&root->right, 70);
+  // addNode(&root->right->right, 85);
 
-  printTree(root);
+  insertElement(&root, 5);
+  insertElement(&root, 3);
+  insertElement(&root, 6);
+  insertElement(&root, 4);
+  insertElement(&root, 8);
+  insertElement(&root, 7);
+  insertElement(&root, 9);
+
+  printTree(root, 0);
   printf("Quantidade de nós: %d\n", countTreeNodes(root));
   printf("Quantidade de folhas: %d\n", countTreeSheets(root));
   printf("Quantidade de não folhas: %d\n", countNotTreeSheets(root));
@@ -108,7 +144,7 @@ int main(void) {
   printf("Valor 5 está na árvore? %s\n", getNodeByValue(root, 5) != NULL ? "Sim" : "Não");
   printf("Valor 5 está na árvore? %s\n", binarySearch(root, 5) != NULL ? "Sim" : "Não");
   clearTree(&root);
-  printTree(root);
+  printTree(root, 0);
 
   return 0;
 }
